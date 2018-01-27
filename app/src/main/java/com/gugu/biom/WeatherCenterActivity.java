01d35209fd7b126e2130be7e567c537cfcd69e5f
pingu <alfodmlzhsks@gmail.com>
2018-01-27 19:52:12 +0900

    Middle Test1 Commit

--- app/src/main/java/com/gugu/biom/WeatherCenterActivity.java
+++ app/src/main/java/com/gugu/biom/WeatherCenterActivity.java
@@ -5,6 +5,8 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.gugu.biom.Fragments.InfoHumidityFragment;
+import com.gugu.biom.Fragments.InfoWindFragment;
 import com.gugu.biom.Fragments.TimeFragment;
 import com.gugu.biom.Fragments.WeekFragmentBottom;
 import com.gugu.biom.Fragments.WeekFragmentTop;
@@ -21,6 +23,8 @@ public class WeatherCenterActivity extends AppCompatActivity {
         ft.add(R.id.llTimeToken, new TimeFragment());
         ft.add(R.id.llWeekTokenTop, new WeekFragmentTop());
         ft.add(R.id.llWeekTokenBottom, new WeekFragmentBottom());
+        ft.add(R.id.llInfoHumidity, new InfoHumidityFragment());
+        ft.add(R.id.llInfoWind, new InfoWindFragment());
         ft.commit();
     }
 }
